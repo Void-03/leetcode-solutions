d017d56ce14fb6df6eed492ca9881d58d7728d37
Sean <161365319+Void-03@users.noreply.github.com>
2024-06-02 13:13:04 +0100

    Time: 2 ms (100.00%), Space: 44.5 MB (73.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,39 @@
+class Solution 
+{
+    public int romanToInt(String s) 
+    {
+        int counter=0;
+        int prev_val=0;
+        for(int i=s.length()-1; i>=0;i--)
+        {
+            char current_char = s.charAt(i);
+            int current_val =romantodecimal(current_char);
+            
+            if(current_val<prev_val)
+            {
+             counter -= current_val;   
+            }
+            else
+            {
+                counter+=current_val;
+            }
+            prev_val = current_val;
+        }
+        return counter;
+    }
+
+    public int romantodecimal(char numeral)
+    {
+        switch(numeral)
+            {
+                case 'I':  return 1;  
+                case 'V':  return 5;  
+                case 'X':  return 10; 
+                case 'L':  return 50; 
+                case 'C':  return 100; 
+                case 'D':  return 500; 
+                case 'M':  return 1000;
+                default :  return 0;
+        }
+    }
+}
