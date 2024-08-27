7449980ac96f38540f6fdd30800df9fde4697df7
Void-03 <161365319+Void-03@users.noreply.github.com>
2024-08-27 16:42:43 +0100

    Time: 1 ms (99.88%), Space: 42 MB (47.20%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -2,12 +2,11 @@ class Solution
 {
     public int scoreOfString(String s) 
     {
-        int count=0;
-
-        for(int i=0; i< s.length()-1;i++)
+        int final_score = 0;
+        for(int i=0; i<s.length()-1;i++)
         {
-            count += Math.abs(s.charAt(i)-s.charAt(i+1));
-        }
-        return count;
+            final_score += Math.abs(s.charAt(i)-s.charAt(i+1));
+        }        
+        return final_score;
     }
 }
